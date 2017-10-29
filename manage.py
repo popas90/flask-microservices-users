0ccf52c2a8cae97cae1e3c7a04206711cb911fe2
popas90 <bogdititupopa@gmail.com>
2017-10-29 17:06:06 +0200

    done Part 1

--- manage.py
+++ manage.py
@@ -1,9 +1,23 @@
 import unittest
+
 from flask_script import Manager
-from project import app, db
+
+from project import create_app, db
+from project.api.models import User
 
 
+app = create_app()
 manager = Manager(app)
+
+
+@manager.command
+def test():
+    """Runs the unit tests without test coverage."""
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
 
 
 @manager.command
@@ -15,13 +29,11 @@ def recreate_db():
 
 
 @manager.command
-def test():
-    """Runs the tests without code coverage."""
-    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
-    result = unittest.TextTestRunner(verbosity=2).run(tests)
-    if result.wasSuccessful():
-        return 0
-    return 1
+def seed_db():
+    """Seeds the database."""
+    db.session.add(User(username='michael', email="michael@realpython.com"))
+    db.session.add(User(username='michaelherman', email="michael@mherman.org"))
+    db.session.commit()
 
 
 if __name__ == '__main__':
